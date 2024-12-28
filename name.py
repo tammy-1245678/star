f00cffdffbd1f72335a67c07223580953dc2a107
tammy-245678 <abdullahbigman38@gmail.com>
2024-12-28 21:20:49 +0500

    commit

---
+++ name.py
@@ -0,0 +1,22 @@
+
+import turtle
+# creating canvas
+turtle.Screen().bgcolor("Orange")
+board = turtle.Turtle()
+board.forward(100) # draw base
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.penup()
+board.right(150)
+board.forward(50)
+# second triangle for star
+board.pendown()
+board.right(90)
+board.forward(100)
+board.right(120)
+board.forward(100)
+board.right(120)
+board.forward(100)
+turtle.done()
